e77dc2b9ac1330147905df6f52e717dd5525f316
Kristopher Tsui <kristophertsui@163.com>
2022-06-07 15:38:10 +0800

    add urls route to views

--- moviereviewsproject/moviereviews/urls.py
+++ moviereviewsproject/moviereviews/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import imp
 from django.contrib import admin
 from django.urls import path
+from movie import views as movieViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', movieViews.home),
+    path('about/', movieViews.about),
 ]
